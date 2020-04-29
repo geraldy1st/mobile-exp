7b99a266623a1c0432c238c178d814011c41d7e2
geraldy1st <geraldy.leondas@gmail.com>
2020-04-29 19:22:14 +0200

    adding header component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
-
-// const ShopPage = () => {
-//   return (
-//     <div>
-//       <div>
-//         <Link to="/">Home</Link>
-//       </div>
-//       <img
-//         style={{
-//           width: '120px',
-//         }}
-//         src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-11-pro-select-2019-family_GEO_EMEA?wid=882&hei=1058&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1567812929188"
-//         alt=""
-//       />
-//       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-//     </div>
-//   );
-// };
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
